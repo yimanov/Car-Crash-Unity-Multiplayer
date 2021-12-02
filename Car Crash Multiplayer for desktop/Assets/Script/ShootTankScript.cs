f2f520af09668a8390bb3f4082617d6f2cf8e1d9
yimanov <biofutureworld@gmail.com>
2021-12-02 12:23:26 -0800

    Submitting the code for photon multiplater

---
+++ Car Crash Multiplayer for desktop/Assets/Script/ShootTankScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootTankScript : MonoBehaviour {
+
+	PhotonView view;
+	public GameObject Bullet;
+	public int force = 30;
+	Rigidbody rb;
+    private bool ShootBool = true;
+
+	// Use this for initialization
+	void Start () {
+		view = GetComponent<PhotonView> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		if (view.isMine && Input.GetKeyDown (KeyCode.Space) && ShootBool) {
+
+            ShootBool = false;
+            StartCoroutine(SetShootBoolBack());
+				view.RPC ("shoot", PhotonTargets.All, transform.Find ("ShootPosition").transform.position, transform.Find ("ShootPosition").transform.rotation);
+ 
+		}
+	}
+
+    IEnumerator SetShootBoolBack()
+    {
+        yield return new WaitForSeconds(0.5f);
+        ShootBool = true;
+
+    }
+
+
+
+    [PunRPC]
+	void shoot(Vector3 Pos, Quaternion quaat)
+	{
+		GameObject GO = Instantiate (Bullet, Pos, quaat) as GameObject;
+		GO.GetComponent<Rigidbody> ().velocity = transform.TransformDirection (Vector3.forward * force);
+	}
+
+
+				}
---
+++ Car Crash Multiplayer for desktop/Assets/Script/StartGameScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGameScript : MonoBehaviour {
+
+	public Button StartGameButton;
+	public GameObject NetworkManagerObject;
+
+	// Use this for initialization
+	void Start () {
+
+		StartGameButton.onClick.AddListener (startGameFunc);
+	}
+	
+	void startGameFunc()
+	{
+		StartGameButton.gameObject.SetActive (false);
+		NetworkManagerObject.SetActive (true);
+	}
+}
